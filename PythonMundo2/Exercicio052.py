430797e82e4e4c5cc74b28462a8ca5ba937ba776
Guilherme <guimcordoba@hotmail.com>
2023-07-15 09:34:35 -0300

    Exercicio 052 atualizado

--- PythonMundo2/Exercicio052.py
+++ PythonMundo2/Exercicio052.py
@@ -1,11 +1,14 @@
 n = int(input("Insira um valor: "))
 soma = 0
-for c in range(1, n):
-    print(c)
-    if n % 2 == 0:
+for c in range(1, n + 1):
+    if n % c == 0:
+        print('\33[33m', end=' ')
         soma += 1
-
+    else:
+        print('\033[31m', end=' ')
+    print(f'{c}', end=' ')
+print(f'\n\033[mO numero {n} foi divisivel {soma} vezes.')
 if soma == 2:
-    print("Numero PRIMO!!!")
+    print("E por isso ele é primo!")
 else:
-    print("O numero não é primo!")
+    print("E por isso ele não é primo!!!")
