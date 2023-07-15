a0c65654672093450bb52abd216e00e34aef8bfb
Guilherme <guimcordoba@hotmail.com>
2023-07-14 21:09:43 -0300

    Exercicio 1 adicionado

---
+++ PythonMundo1/Exercicio001,py.py
@@ -0,0 +1,6 @@
+'''a = int(input('Informe um valor: '))
+b = int(input('Informe outro valor: '))
+print('Valores informados \33[35m{}\033[m e \033[36m{}\033[m!!!'.format(a, b))'''
+
+nome = input('Informe seu nome: ')
+print('Muito prazer {}{}{}!!!'.format('\033[4m', nome , '\033[m'))
