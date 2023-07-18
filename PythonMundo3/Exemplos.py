1cd1cc8bcb540662d9a504cf08a479b7a708bbff
Guilherme <guimcordoba@hotmail.com>
2023-07-18 17:06:02 -0300

    finalizando os exemplos

---
+++ PythonMundo3/Exemplos.py
@@ -0,0 +1,24 @@
+#TUPLAS SÃO IMUTÁVEIS
+'''lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata frita')
+print(lanche)'''
+
+'''for c in lanche:
+    print(f'Eu vou comer {c}')'''
+
+'''for c in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[c]}, na posição {c}')'''
+
+'''for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida}, na posição {pos}')'''
+
+#print(sorted(lanche))
+
+'''a = (2, 5, 4)
+c = (5, 8, 1, 2)
+c = a + c
+print(c.count(5)) #conta o numero de ocorrencias
+print(c.index(5)) #mostra a posição'''
+
+pessoa = ('Guilherme', 39, 'M', 99.88)
+print(pessoa)
+del(pessoa)
