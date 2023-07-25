f45a50647e194349b38c1b43cee23cdb3cdd1c5d
Guilherme <guimcordoba@hotmail.com>
2023-07-25 16:16:09 -0300

    Exercicio 080 adicionado

---
+++ PythonMundo3/Exercicio080.py
@@ -0,0 +1,17 @@
+'''Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()).
+ No final, mostre a lista ordenada na tela.'''
+valores = list()
+
+for i in range(0, 5):
+    n = int(input('Insira um valor: '))
+    if i == 0 or n > valores[-1]:
+        valores.append(n)
+    else:
+        pos = 0
+        while pos < len(valores):
+            if n <= valores[pos]:
+                valores.insert(pos, n)
+                break
+print('-'*55)
+print(f'Os valores digitados em ordem foram: {valores} |')
+print('-'*55)
