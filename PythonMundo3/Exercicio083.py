ea491d04e4e1120fa985fd62257b233250fb57ab
Guilherme <guimcordoba@hotmail.com>
2023-07-25 17:03:31 -0300

    Exercicio 083 adicionado

---
+++ PythonMundo3/Exercicio083.py
@@ -0,0 +1,16 @@
+'''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
+expre = str(input('Insira um expressão: '))
+lista = []
+for simb in expre:
+    if simb == '(':
+        lista.append('(')
+    elif simb == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+if len(lista) == 0:
+    print('Sua expressão está CORRETA!')
+else:
+    print('Sua expressão não INCORRETA!!')
