79733eca2383f976d7377b10b8bc097661acc3ac
Guilherme <guimcordoba@hotmail.com>
2023-07-29 17:08:40 -0300

    Exercicio 084 terminado por completo

--- PythonMundo3/Exercicio084.py
+++ PythonMundo3/Exercicio084.py
@@ -3,34 +3,30 @@ A) Quantas pessoas foram cadastradas, B) Uma listagem com as pessoas mais pesada
 
 pessoas = list()
 dados = list()
-maiorpe = list()
-menorpe = list()
-count = maiorpeso = menorpeso = 0
+count = maiorpeso = menorpeso  = 0
 while True:
     dados.append(str(input('Nome: ')))
     dados.append(int(input('Peso (kg):')))
+    if len(pessoas) == 0:
+        maiorpeso = menorpeso = dados[1]
+    else:
+        if dados[1] > maiorpeso:
+            maiorpeso = dados[1]
+        if dados[1] < menorpeso:
+            menorpeso = dados[1]
     pessoas.append(dados[:])
-    count += 1
     dados.clear()
     op = str(input('Deseja continuar [S/N]: ')).upper()
     if op == 'N':
         break
+
+
+print(f'Foram cadastradas {len(pessoas)} pessoas.')
+print(f'O maior peso é {maiorpeso}. Peso de ', end='')
 for p in pessoas:
-    if p == 0:
-        maiorpeso = p[1]
-        menorpeso = p[1]
-        maiorpe.append(p[0])
-        maiorpe.append(p[0])
-    else:
-        if p[1] < menorpeso:
-            menorpeso = p[1]
-            maiorpe.append(p[0])
-        if p[1] > maiorpeso:
-            maiorpeso = p[1]
-            maiorpe.append(p[0])
-print(f'Foram cadastradas {count} pessoas.')
-for p in maiorpe:
-    print(f'O maior peso é {maiorpeso}. Peso de {maiorpe}')
-for p in menorpe:
-    print(f'O menor peso é {menorpeso}, Peso de {menorpe}')
-print(f'Foram cadastradas {count} pessoas.')
+    if p[1] == maiorpeso:
+        print(f'[{p[0]}]', end=' ')
+print(f'\nO menor peso é {menorpeso}, Peso de ', end='')
+for p in pessoas:
+    if p[1] == menorpeso:
+        print(f'[{p[0]}]', end=' ')
